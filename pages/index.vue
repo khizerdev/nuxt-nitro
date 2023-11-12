9446c5fe97a5cd28e93e90b28413ab346d81dafd
khizerdev <46632721+khizerdev@users.noreply.github.com>
2023-11-12 15:14:11 +0500

    Managing API Errors

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,16 @@
 <script setup>
-const { data } = await useFetch("/api/1");
+const { data, error } = await useFetch("/api/150");
 const { data: todos } = await useFetch("https://dummyjson.com/todos");
 </script>
 
 <template>
   <h1>Home Page for Nuxt 3 Server Course</h1>
-  <pre>
+  <pre v-if="data">
     {{ data }}
   </pre>
+  <div v-else>
+    <p>{{ error }}</p>
+  </div>
   <pre>
     {{ todos }}
   </pre>
--- server/api/[todo].ts
+++ server/api/[todo].ts
@@ -1,14 +1,21 @@
 export default defineEventHandler(async (event) => {
-  const { todo } = event.context.params;
-  const response = await $fetch(`https://dummyjson.com/todos/${todo}`);
+  try {
+    const { todo } = event.context.params;
+    const response = await $fetch(`https://dummyjson.com/todos/${todo}`);
 
-  console.log(event.context);
+    console.log(event.context);
 
-  const idealResponse = {
-    id: response.id,
-    todo: response.todo,
-    completed: response.completed,
-  };
+    const idealResponse = {
+      id: response.id,
+      todo: response.todo,
+      completed: response.completed,
+    };
 
-  return idealResponse;
+    return idealResponse;
+  } catch (error) {
+    throw createError({
+      statusCode: 404,
+      message: "Can't find the todo!",
+    });
+  }
 });
